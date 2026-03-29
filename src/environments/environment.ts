03766c570de6a81e9001999ecbfb9795298c2d87
Ahmed Reda <a7med.reda.19002@gmail.com>
2026-03-29 19:31:37 +0200

    Restore To Previous Update

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   baseUrl : "https://ecommerce.routemisr.com/api/v1/",
-  ecommURL : "https://fresh-cart-e-commerce-gamma.vercel.app/allorders"
+  ecommURL : "http://localhost:4200"
 };
